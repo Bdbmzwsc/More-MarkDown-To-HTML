32eefa1aaf0f2bf365963fad09c10bdfe7f1361f
Bdbmzwsc <88311279+Bdbmzwsc@users.noreply.github.com>
2021-10-17 17:15:32 +0800

    完善API
    完整只要界面和API

---
+++ API.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace MMTH
+{
+    class API
+    {
+        #region [ API: 记事本 ]
+
+        /// <summary>
+        /// 传递消息给记事本
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="Msg"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("User32.DLL")]
+        public static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, string lParam);
+
+        /// <summary>
+        /// 查找句柄
+        /// </summary>
+        /// <param name="hwndParent"></param>
+        /// <param name="hwndChildAfter"></param>
+        /// <param name="lpszClass"></param>
+        /// <param name="lpszWindow"></param>
+        /// <returns></returns>
+        [DllImport("User32.DLL")]
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        /// <summary>
+        /// 记事本需要的常量
+        /// </summary>
+        public const uint WM_SETTEXT = 0x000C;
+        public string a;
+        #endregion
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Markdig;
 using System.IO;
+using System.Runtime.InteropServices;
 
 namespace MMTH
 {
@@ -36,14 +37,49 @@ namespace MMTH
 
         private void yulan_Click(object sender, RoutedEventArgs e)
         {
+            /*
             File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"simple.html", mth(MT.Text));
             web1.Navigate(AppDomain.CurrentDomain.BaseDirectory + @"simple.html");
+            */
+            string str = "";
+            System.Diagnostics.Process Proc=new System.Diagnostics.Process();
+            try
+            {
+                str = File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + @"simple.html");
+                // 启动记事本
+                //  Proc = new System.Diagnostics.Process();
+                Proc.StartInfo.FileName = "notepad.exe";
+                Proc.StartInfo.UseShellExecute = false;
+                Proc.StartInfo.RedirectStandardInput = true;
+                Proc.StartInfo.RedirectStandardOutput = true;
+
+                Proc.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro:\r\n" + ex.Message);
+            }
+            if (Proc != null)
+            {
+                // 调用 API, 传递数据
+                while (Proc.MainWindowHandle == IntPtr.Zero)
+                {
+                    Proc.Refresh();
+                }
+                
+                IntPtr vHandle = API.FindWindowEx(Proc.MainWindowHandle, IntPtr.Zero, "Edit", null);
+
+                // 传递数据给记事本
+                API.SendMessage(vHandle, API.WM_SETTEXT, 0, str);
+            }
+
         }
         //初始化
         public void chu()
         {
-            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"simple"))
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + @"simple.html"))
             {
+                File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"simple.html", "<h1>Welcome to MMTH</h1>");
                 web1.Navigate(AppDomain.CurrentDomain.BaseDirectory + @"simple.html");
                 return;
                 
@@ -62,6 +98,12 @@ namespace MMTH
                web1.Navigate(AppDomain.CurrentDomain.BaseDirectory + @"simple.html");
 
             }
+        }
+
+        private void MT_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + @"simple.html", mth(MT.Text));
+            web1.Navigate(AppDomain.CurrentDomain.BaseDirectory + @"simple.html");
         }
     }
 }
